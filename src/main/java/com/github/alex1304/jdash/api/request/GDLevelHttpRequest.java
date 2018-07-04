8e83614d2c0298be7788c097f1ec18bc40cfdf1d
Alex1304 <mirandaa1304@gmail.com>
2018-07-04 18:27:15 +0200

    Fixed wrong cipher function for levelpass in gdlevelhttprequest

--- src/main/java/com/github/alex1304/jdash/api/request/GDLevelHttpRequest.java
+++ src/main/java/com/github/alex1304/jdash/api/request/GDLevelHttpRequest.java
@@ -44,7 +44,7 @@ public class GDLevelHttpRequest extends GDHttpRequest<GDLevel> {
 			else if (mappedLevelData.get(Constants.INDEX_LEVEL_PASS).equals("0"))
 				pass = -1;
 			else
-				pass = Integer.parseInt(RobTopsWeakCrypto.decodeGDAccountPassword(mappedLevelData.get(Constants.INDEX_LEVEL_PASS)));
+				pass = Integer.parseInt(RobTopsWeakCrypto.decodeLevelPass(mappedLevelData.get(Constants.INDEX_LEVEL_PASS)));
 			
 			return new GDLevel(
 				Long.parseLong(mappedLevelData.get(Constants.INDEX_LEVEL_ID)),
