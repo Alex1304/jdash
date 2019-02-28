251960061df37334375a1b3e81ae8dff9b6e29d9
Alex1304 <mirandaa1304@gmail.com>
2019-02-28 19:47:52 +0100

    Fixed javadoc errors (again)

--- src/main/java/com/github/alex1304/jdash/util/Gzip.java
+++ src/main/java/com/github/alex1304/jdash/util/Gzip.java
@@ -6,7 +6,7 @@ import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 /**
- * @see https://myadventuresincoding.wordpress.com/2016/01/02/java-simple-gzip-utility-to-compress-and-decompress-a-string/
+ * https://myadventuresincoding.wordpress.com/2016/01/02/java-simple-gzip-utility-to-compress-and-decompress-a-string/
  */
 public class Gzip {
 
