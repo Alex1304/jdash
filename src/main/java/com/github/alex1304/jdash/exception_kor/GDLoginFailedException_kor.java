7501fd726112270ce9e2043bc3969acc5fc32639
kimtaeyoung <kty4119@naver.com>
2019-05-20 01:17:58 +0900

    Return to original condition

--- src/main/java/com/github/alex1304/jdash/exception_kor/GDLoginFailedException_kor.java
+++
@@ -1,9 +0,0 @@
-package com.github.alex1304.jdash.exception;
-public final class GDLoginFailedException extends Exception
-{
-private static final long serialVersionUID = 5819598770234742861L;
-	public GDLoginFailedException(Throwable cause)
-{
-super(cause);
-}
-}
