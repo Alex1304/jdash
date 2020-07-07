11dc41836909f0ccd94e06e81859797f85320d2e
DenFade <rurchi1206@gmail.com>
2020-07-07 18:43:19 +0900

    Renamed to CommentSortMode

--- src/main/java/com/github/alex1304/jdash/util/CommentSortMode.java
+++ src/main/java/com/github/alex1304/jdash/util/CommentSortMode.java
@@ -1,12 +1,12 @@
 package com.github.alex1304.jdash.util;
 
-public enum LevelCommentFilter {
+public enum CommentSortMode {
     RECENT("0"),
     MOST_LIKED("1");
 
     private final String val;
 
-    LevelCommentFilter(String val){
+    CommentSortMode(String val){
         this.val = val;
     }
 
