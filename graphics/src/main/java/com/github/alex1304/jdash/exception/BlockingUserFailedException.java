3cbf111ea3cedc77dc6b863e2864258e83965119
Alex1304 <mirandaa1304@gmail.com>
2021-05-20 03:01:55 +0200

    Removed unused files from merge

--- graphics/src/main/java/com/github/alex1304/jdash/exception/BlockingUserFailedException.java
+++
@@ -1,5 +0,0 @@
-package com.github.alex1304.jdash.exception;
-
-public final class BlockingUserFailedException extends GDClientException {
-    private static final long serialVersionUID = 8501238572375382595L;
-}
--- graphics/src/main/java/com/github/alex1304/jdash/exception/RatingFailedException.java
+++
@@ -1,5 +0,0 @@
-package com.github.alex1304.jdash.exception;
-
-public final class RatingFailedException extends GDClientException {
-    private static final long serialVersionUID = 2381053890202543576L;
-}
