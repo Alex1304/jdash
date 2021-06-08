8bbf3eff673d5119e604893ca9457ecd15ce78d2
Alex1304 <mirandaa1304@gmail.com>
2021-06-08 14:41:18 +0200

    Properly export jdash events packages

--- events/src/main/java/module-info.java
+++ events/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module jdash.events {
 
+    exports jdash.events;
+    exports jdash.events.object;
+    exports jdash.events.producer;
+
     requires reactor.extra;
     requires transitive jdash.client;
     requires static org.immutables.value;
